6a45ce424e1a72d5c2f7972230510566603ce5fb
alexispqa <ppaax1@gmail.com>
2020-12-21 17:43:57 +0700

    added Design of utility for button & textfield

--- TicketBooking/ViewController/SignUpViewController.swift
+++ TicketBooking/ViewController/SignUpViewController.swift
@@ -18,18 +18,15 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         signUpBtn.setTitle("Sign up   \u{2794}", for: .normal)
-
+        Utilities.styleTextField(fullNameTextField)
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(phoneNumberTextField)
+        Utilities.styleTextField(passwordTextField)
+        Utilities.styleTextField(addressTextField)
+        Utilities.styleFilledButton(signUpBtn)
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
