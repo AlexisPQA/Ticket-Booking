bb2b4f228da5a6df88a94805617936349ab4ed38
alexispqa <ppaax1@gmail.com>
2020-12-21 15:26:21 +0700

    rename to loginviewcontroller

---
+++ TicketBooking/ViewController/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TicketBooking
+//
+//  Created by AlexisPQA on 12/19/20.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        signInBtn.setTitle("Sign in   \u{2794}", for: .normal)
+    }
+
+    @IBAction func signInTapped(_ sender: Any) {
+    }
+    
+    @IBAction func signUpTapped(_ sender: Any) {
+    }
+}
+
