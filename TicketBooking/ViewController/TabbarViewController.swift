a5055675a3446d8f9ac6e476a32e758d589734fe
AlexisPQA <ppaax1@gmail.com>
2020-12-26 00:39:29 +0700

    changed some setting shadow tabbar

--- TicketBooking/ViewController/TabbarViewController.swift
+++ TicketBooking/ViewController/TabbarViewController.swift
@@ -14,16 +14,15 @@ class TabbarViewController: UITabBarController {
         self.tabBar.clipsToBounds = true // remove top line of tabbar
         let layer = CAShapeLayer()
         layer.path = UIBezierPath(roundedRect: CGRect(x: 30, y: 10, width: self.tabBar.bounds.width - 60, height: 67), cornerRadius:20).cgPath
-        layer.shadowColor = UIColor.lightGray.cgColor
-        layer.shadowOffset = CGSize(width: 5.0, height: 5.0)
-        layer.shadowRadius = 25.0
-        layer.shadowOpacity = 0.3
+        layer.shadowColor = UIColor.init(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.16).cgColor
+        layer.shadowOffset = CGSize(width: 1, height: 1)
+        layer.shadowRadius = 5
+        layer.shadowOpacity = 1
         layer.borderWidth = 1.0
         layer.opacity = 1.0
         layer.isHidden = false
         layer.masksToBounds = false
         layer.fillColor = UIColor(red: 219/255, green: 229/255, blue: 243/255, alpha: 1).cgColor
-  
         self.tabBar.layer.insertSublayer(layer, at: 0)
         
     }
